50557c152dbbb45f03ce6e83d0a92d082fce4afb
ayyuan <alchyuan@gmail.com>
2023-09-08 14:15:26 -0700

    chromatic abberation

--- src/draw/postprocessing/createQuadProgram.ts
+++ src/draw/postprocessing/createQuadProgram.ts
@@ -27,9 +27,33 @@ in vec2 vPosition;
 out vec4 outColor;
 
 void main() {
-  vec2 p = 0.5*vPosition + 0.5;
-  vec4 c = texture(uColor, p);
-  outColor = c;
+  vec2 uv = 0.5*vPosition + 0.5;
+
+  // radial chromatic abberation from: https://www.shadertoy.com/view/Mtf3zl
+  float distFromCenter = length( uv - vec2( 0.5, 0.5 ) );
+  //TODO:
+  float shift = 0.0; //0.05 * shake(iChannel1);
+
+  vec2 uvR, uvG, uvB;
+  vec4 color;
+
+  // outer edges have more aberration
+  shift *= min(distFromCenter, 0.3);
+
+  // vector from center to px
+  vec2 colorVec = normalize(uv - vec2(0.5,0.5));
+
+  // color shift in the dir of colorVec
+  uvR = vec2(uv.x-(colorVec.x*shift), uv.y-(colorVec.y*shift));
+  uvG = vec2(uv.x                   , uv.y);
+  uvB = vec2(uv.x+(colorVec.x*shift), uv.y+(colorVec.y*shift));
+
+  color.r = texture( uColor, uvR ).r;
+  color.g = texture( uColor, uvG ).g;
+  color.b = texture( uColor, uvB ).b;
+  color.a = 1.0;
+
+  outColor = color;
 }
 `;
 
